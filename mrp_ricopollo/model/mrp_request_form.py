d711199a7ff60f4959b2928700e82dafc8f32d9c
brianle <lehoangan1988@gmail.com>
2015-04-27 00:21:22 +0700

    mrp task 01

---
+++ mrp_ricopollo/model/mrp_request_form.py
@@ -0,0 +1,103 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2004-2008 Tiny SPRL (<http://tiny.be>). All Rights Reserved
+#    $Id$
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from openerp.osv import fields, osv
+from openerp.tools.translate import _
+import time
+
+class mrp_request_form(osv.osv):
+    _name = 'mrp.request.form'
+    _inherit = ['mail.thread']
+
+    _columns ={        
+        'name': fields.char('Description', 100, readonly=True, states={'draft': [('readonly', False)]}),
+        'ref': fields.char('Ref', 100, readonly=True, states={'draft': [('readonly', False)]}),
+        'warehouse_id': fields.many2one('stock.warehouse', 'Farm Name', required=True, readonly=True, states={'draft': [('readonly', False)]}),
+        'warehouse_to_id': fields.many2one('stock.warehouse', 'Recieve Request', required=True, readonly=True, states={'draft': [('readonly', False)]}),
+        'date': fields.date('Date Request', required=True, readonly=True, states={'draft': [('readonly', False)]}),
+        'user_id': fields.many2one('res.users', 'Create By', readonly=True),
+        'validate_date': fields.date('Date Validate', readonly=True),
+        'validate_user_id': fields.many2one('res.users', 'Validate By', readonly=True),
+        'line_ids': fields.one2many('mrp.request.form.line', 'request_id', 'Detail Request', readonly=True, states={'draft': [('readonly', False)]}),
+        'state': fields.selection([
+            ('draft', 'Draft Quotation'),
+            ('approve', 'Approved'),
+            ('cancel', 'Cancelled'),
+            ('mo', 'MO Created'),
+            ], 'Status', readonly=True,select=True),
+    }
+
+    _defaults= {
+        'state': 'draft',
+        'user_id': lambda self,cr,uid,context=None: uid,
+        'date': time.strftime('%Y-%m-%d'),
+    }
+    def action_cancel(self, cr, uid, ids, context=None):
+        return self.write(cr, uid, ids, {'state': 'cancel',})
+
+    def action_set_to_draft(self, cr, uid, ids, context=None):
+        return self.write(cr, uid, ids, {'state': 'draft',})
+
+    def action_approve(self, cr, uid, ids, context=None):
+        return self.write(cr, uid, ids, {'state': 'approve',
+                                        'validate_user_id': uid,
+                                        'validate_date': time.strftime('%Y-%m-%d')})
+    
+    def action_make_mo(self, cr, uid, ids, context=None):
+        production_obj = self.pool.get('mrp.production')
+        bom_obj = self.pool.get('mrp.bom')
+        product_uom_obj = self.pool.get('product.uom')
+        dict_product = {}
+        warehouse_to_id = False
+        for obj in self.browse(cr, uid, ids, context):
+            if not warehouse_to_id:
+                warehouse_to_id = obj.warehouse_to_id
+            elif warehouse_to_id != obj.warehouse_to_id:
+                raise osv.except_osv(_("Invalid Action!"), _("Selected the same recieve place."))
+            for line in obj.line_ids:
+                q = product_uom_obj._compute_qty(cr, uid, line.uom_id.id, line.qty_unit, line.product_id.uom_id.id)
+                name = '%s:%s'%(obj.name, obj.ref)
+                if line.product_id.id not in dict_product.keys():
+                    dict_product.update({line.product_id.id: [q, [name], line.product_id.uom_id.id]})
+                else:
+                    dict_product[line.product_id.id][0] += q
+                    if name not in dict_product[line.product_id.id][1]:
+                        dict_product[line.product_id.id][1] += [name]
+        for product_id in dict_product.keys():
+            bom_id = bom_obj._bom_find(cr, uid, product_id=product_id,
+                                                    properties=[], context=context)
+            if bom_id:
+                bom = bom_obj.browse(cr, uid, bom_id, context=context)
+                routing_id = bom.routing_id.id
+                produce_id = production_obj.create(cr, uid, {
+                    'origin': dict_product[product_id][1],
+                    'product_id': product_id,
+                    'product_qty': dict_product[product_id][0],
+                    'product_uom': dict_product[product_id][2],
+                    'location_src_id': warehouse_to_id.lot_stock_id.id,
+                    'location_dest_id': warehouse_to_id.lot_stock_id.id,
+                    'bom_id': bom_id,
+                    'routing_id': routing_id,
+                    'date_planned': time.strftime('%Y-%m-%d %H:%M:%S'),
+                })
+
+        return self.write(cr, uid, ids, {'state': 'mo'})
---
+++ mrp_ricopollo/model/mrp_request_form_line.py
@@ -0,0 +1,51 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2004-2008 Tiny SPRL (<http://tiny.be>). All Rights Reserved
+#    $Id$
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from openerp.osv import fields, osv
+from openerp.tools.translate import _
+import time
+
+class mrp_request_form_line(osv.osv):
+    _name = 'mrp.request.form.line'
+    _columns ={
+        'product_id': fields.many2one('product.product', 'Product Requested', required=True),
+        'request_id': fields.many2one('mrp.request.form', 'Parent', required=True,ondelete='cascade'),
+        'qty_qq': fields.float('Quantity in quintals (qq)', required=True),
+        'qty_unit': fields.float('Quantity Unit', required=True),
+        'uom_id': fields.many2one('product.uom', 'UoM', required=True),
+    }
+
+    def onchange_product_id(self, cr, uid, ids, product_id, qty_qq, context=None):
+        if product_id:
+            prod_obj = self.pool.get('product.product').browse(cr, uid, product_id, context)
+            uom_id = prod_obj.uom_id.id
+            qty_unit = qty_qq * 46
+            return {'value': {'product_id': product_id,
+                              'qty_qq': qty_qq,
+                              'qty_unit': qty_unit,
+                              'uom_id': uom_id}}
+        return {'value': {'product_id': False,
+                          'qty_qq': 0,
+                          'qty_unit': 0,
+                          'uom_id': False}}
+
+ 
