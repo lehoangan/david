6dfd3e5f6b7357fd52a6fa978fecaf90af7ed4c6
anlee <anlh@init.vn>
2015-06-23 14:50:14 +0700

    Task 43 Customer average sales report

--- report_sale_uos/report/daily_average_sales.py
+++ report_sale_uos/report/daily_average_sales.py
@@ -219,7 +219,7 @@ class Parser(report_sxw.rml_parse):
                 count = 1
                 date = datetime.datetime.strptime(tmp['date_invoice'], DEFAULT_SERVER_DATE_FORMAT)
                 date_old_qty = []
-                while count <= 4:
+                while count < 4:
                     count += 1
                     date = date - datetime.timedelta(days=7)
                     date_old_qty += ['%s'%date.strftime(DEFAULT_SERVER_DATE_FORMAT)]
@@ -240,10 +240,10 @@ class Parser(report_sxw.rml_parse):
                             product['product_id'])
                 self.cr.execute(select_str)
                 old = self.cr.dictfetchone()
-                old_qty = 0
+                old_qty = tmp['unit_qty']
                 if old and old['unit_qty']:
                     old_qty += old['unit_qty']
-                dict_date.update({tmp['date_invoice']: [old_qty,tmp['unit_qty']]})
+                dict_date.update({tmp['date_invoice']: [round(old_qty/4, 2), tmp['unit_qty']]})
 
 
             result.append({
